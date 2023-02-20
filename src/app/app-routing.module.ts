7db0975bb67a49adb0dbfb25209f3d4783b9c792
kahlaoui <kahlawidhekra@gmail.com>
2023-02-20 14:23:38 +0100

    premier sauvgarde

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'acceuil-component', component: AcceuilComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AcceuilComponent
   ],
   imports: [
     BrowserModule,
